efe31279bde0373c3af1c59d71e996cc0e4d81ac
ll <uiltloq@gmail.com>
2024-06-15 02:17:05 +0300

    update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -65,7 +65,7 @@ export const ProductList = () => {
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
+                text: `Купить ${getTotalPrice(newItems).toLocaleString('ru')}`
             })
         }
     }
